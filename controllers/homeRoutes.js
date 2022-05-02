c499e5cdd3db0698bca55092f880374df8ef393e
NicoleD1999 <nmd1018@yahoo.com>
2022-05-02 10:45:00 -0500

    added initial routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      const posts = dbPostData.map((post) =>
+        post.get({ plain: true })
+      );
+  
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
