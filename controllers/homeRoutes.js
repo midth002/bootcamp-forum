eafd4a41b8c337d986efb18e558a36fdadc9a7a9
Andrew Midthun <midth002@gmail.com>
2022-05-02 14:06:31 -0500

    Starting heroku app

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,23 +3,20 @@ const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-      const dbPostData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
+      // const dbPostData = await Post.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['username'],
+      //     },
+      //   ],
+      // });
   
-      const posts = dbPostData.map((post) =>
-        post.get({ plain: true })
-      );
+      // const posts = dbPostData.map((post) =>
+      //   post.get({ plain: true })
+      // );
   
-      res.render('home', {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
+      res.render('home');
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
