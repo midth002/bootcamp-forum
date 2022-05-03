6371ac354ffec94fc0dd4b545e426d26a1f49a6c
NicoleD1999 <nmd1018@yahoo.com>
2022-05-03 13:32:49 -0500

    working signup feature

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -23,12 +23,45 @@ router.post('/', async (req, res) => {
     }
 
     // Session variables based on the current logged in user
-    req.session.save(() => {
-      req.session.user_id = user.id;
-      req.session.logged_in = true;
+    // req.session.save(() => {
+    //   req.session.user_id = user.id;
+    //   req.session.logged_in = true;
       
-      res.json(User);
-    });
+    //   res.json(User);
+    // });
+    res.render('login')
+    res.json({message: `Hello`})
+
+
+  } catch (error) {
+    res.status(400).json(error);
+    console.log(error)
+  }
+});
+
+router.get('/signup', (req, res)=> {
+  res.render('signup')
+})
+
+router.post('/signup', async (req, res) => {
+  try {
+    // Find the user who matches with the username in the database
+    const user = await User.findOne({ where: {user_name: req.body.user_name }});
+
+    // If there is no match with the username, send a incorrect message to the user and have them retry
+    if (!user) {
+      try {
+        const createUser = await User.create({
+          ...req.body, 
+        });
+        //figure out how to use nodemailer
+        res.json({message: `User created`})
+      } catch(err){
+        res.status(500).json(err)
+      }
+    } else {
+      res.status(400).json({message: 'Please make sure password is at least 8 characters long and make sure username does not already exist'})
+    }
 
 
   } catch (error) {
