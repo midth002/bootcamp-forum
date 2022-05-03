53073e64be7185d76024c34e72feda0d6f7db260
Andrew Midthun <midth002@gmail.com>
2022-05-03 14:20:23 -0500

    Got the login functionality working.

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,21 @@
 const loginHandler = async (event) => {
     event.preventDefault();    
-    const usernameLog = $('#login-username').trim();
-    const passwordLog = $('#login-password').trim();
+    const email = $('#login-username').val().trim();
+    const password = $('#login-password').val().trim();
     // if email and password have been entered
     if(email && password) {
         const response = await fetch('/api/login', {
             method: 'POST',
-            body: JSON.stringify({ usernameLog, passwordLog }),
+            body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json '},
         });
         if (response.ok) {
             // replace login page with home page
             document.location.replace('/');
+            
         } else {
             alert('You are unable to login');
         }
     }
 };
-document    
-    $('#login-form')
-    .on('submit', loginHandler);
+$('#login-form').on('submit', loginHandler);
