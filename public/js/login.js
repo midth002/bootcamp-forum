99a938031d3227e7871a621ca6dacc19274ab900
Andrew Midthun <midth002@gmail.com>
2022-05-03 13:34:55 -0500

    Save

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,17 @@
 const loginHandler = async (event) => {
     event.preventDefault();    
-    const username = $('#login-username').trim();
-    const password = $('#login-password').trim();
+    const usernameLog = $('#login-username').trim();
+    const passwordLog = $('#login-password').trim();
     // if email and password have been entered
-    if(username && password) {
+    if(email && password) {
         const response = await fetch('/api/login', {
             method: 'POST',
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ usernameLog, passwordLog }),
             headers: { 'Content-Type': 'application/json '},
         });
         if (response.ok) {
             // replace login page with home page
-            // document.location.replace('/');
-            alert('success')
+            document.location.replace('/');
         } else {
             alert('You are unable to login');
         }
