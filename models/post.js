3371bea8c15e2b72baf566ed0ff8f41d3d22b3b8
NicoleD1999 <nmd1018@yahoo.com>
2022-05-04 13:52:53 -0500

    added post handlebars

--- models/post.js
+++ models/post.js
@@ -39,7 +39,7 @@ Post.init (
                 model: 'user',
                 key:'id'
             },
-        },
+        },  
     },
 
     {
@@ -48,7 +48,7 @@ Post.init (
         freezeTableName: true,
         underscored: true,
         modelName: 'post',
-    }
+    },
 );
 
 
